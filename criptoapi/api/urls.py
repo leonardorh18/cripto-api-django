1a5541f85d7042871124c6363edd7395573508c5
Leonardo Rocha <leoheiro@hotmail.com>
2021-08-31 10:18:09 -0300

    mais rotas de api

--- criptoapi/api/urls.py
+++ criptoapi/api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [ 
 
     path('', views.home, name = 'home'),
-    path('getprice/<str:cripto>', views.getPrice)
+    path('getprice/<str:cripto>', views.getPrice, name = 'getPrice'),
+    path('getAllinfo/<str:cripto>', views.getAllinfo, name = 'getAllinfo')
       
 ]
--- criptoapi/api/views.py
+++ criptoapi/api/views.py
@@ -1,8 +1,10 @@
 from django.http import response
 from django.shortcuts import render, redirect
 import requests
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from bs4 import BeautifulSoup
+import re
+
 
 # Create your views here.
 
@@ -10,23 +12,67 @@ def home(req):
     
     return render(req, 'home.html')
 
+
 def getPrice(req, cripto):
 
     
     print('>>>>>>>>>>>>>>>>>>>>>', cripto)
-    url = 'https://markets.businessinsider.com/currencies/'+cripto+'-usd'
-    html =  requests.get(url)
+    try:
+        url = 'https://markets.businessinsider.com/currencies/'+cripto+'-usd'
+        html =  requests.get(url)
 
-    bs = BeautifulSoup(html.content, 'html.parser')
-    
-    price = bs.find('span', {'class': 'price-section__current-value'})
-    #coin = bs.findAll('h2', {'clas': 'sc-1q9q90x-0 jCInrl h1'})
-    price = price.text
-    print('price-----------------------', price)
+        bs = BeautifulSoup(html.content, 'html.parser')
+        
+        price = bs.find('span', {'class': 'price-section__current-value'})
+        #coin = bs.findAll('h2', {'clas': 'sc-1q9q90x-0 jCInrl h1'})
+        price = price.text
+        print('price-----------------------', price)
+
+    except Exception as e:
+        raise Http404("Cannot get price of "+cripto)
+
 
     response = {
         'price': price,
-        'coin': cripto,
+        'cur': 'usd'
     }
+
+    return JsonResponse(response)
+
+def getAllinfo(req, cripto):
+    
+    
+    print('>>>>>>>>>>>>>>>>>>>>>', cripto)
+    response = {}
+    try:
+        url = 'https://markets.businessinsider.com/currencies/'+cripto+'-usd'
+        html =  requests.get(url)
+
+        bs = BeautifulSoup(html.content, 'html.parser')
+        
+        price = bs.find('span', {'class': 'price-section__current-value'})
+        #coin = bs.findAll('h2', {'clas': 'sc-1q9q90x-0 jCInrl h1'})
+        price = price.text
+        #print('price-----------------------', price)
+        response['price'] = price
+        response['cur'] = 'usd'
+        snapshot = bs.find('div', {'class': 'snapshot'})
+        data_items = snapshot.findAll('div', {'class': 'snapshot__data-item'})
+        for data in data_items:
+            data = data.text.split('\n')[1::]
+
+            for i in range(len(data)):
+                data[i] = re.sub('[\t*\r]', '', data[i])
+                
+            data = data[0:-1]
+            #print(data)
+            #print('-------------')
+            response[data[1]] = data[0]
+        
+    
+    except Exception as e:
+        raise Http404("Cannot get price of "+cripto)
+
+
 
     return JsonResponse(response)
