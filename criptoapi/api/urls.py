d9bd1b26d894f197d6620aeec6c2784b4c694262
Leonardo Rocha <leoheiro@hotmail.com>
2021-08-30 22:04:04 -0300

    getprice/<cripto>  usando scrapper no b insider

--- criptoapi/api/urls.py
+++ criptoapi/api/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [ 
 
     path('', views.home, name = 'home'),
-    
+    path('getprice/<str:cripto>', views.getPrice)
       
 ]
--- criptoapi/api/views.py
+++ criptoapi/api/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.http import response
+from django.shortcuts import render, redirect
+import requests
+from django.http import HttpResponse, JsonResponse
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 def home(req):
     
     return render(req, 'home.html')
+
+def getPrice(req, cripto):
+
+    
+    print('>>>>>>>>>>>>>>>>>>>>>', cripto)
+    url = 'https://markets.businessinsider.com/currencies/'+cripto+'-usd'
+    html =  requests.get(url)
+
+    bs = BeautifulSoup(html.content, 'html.parser')
+    
+    price = bs.find('span', {'class': 'price-section__current-value'})
+    #coin = bs.findAll('h2', {'clas': 'sc-1q9q90x-0 jCInrl h1'})
+    price = price.text
+    print('price-----------------------', price)
+
+    response = {
+        'price': price,
+        'coin': cripto,
+    }
+
+    return JsonResponse(response)
